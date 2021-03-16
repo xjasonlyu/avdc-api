3e8c880998b47e5d69f04ffe2788ba735d795db5
xjasonlyu <xjasonlyu@gmail.com>
2021-03-16 22:15:39 +0800

    refactor: avsox

--- avdc/provider/avsox.py
+++ avdc/provider/avsox.py
@@ -2,28 +2,14 @@ from bs4 import BeautifulSoup
 from lxml import etree
 
 from avdc.utility.httpclient import get_html
-from avdc.utility.metadata import toMetadata
-
-
-def getActorPhoto(content: str) -> dict:  # //*[@id="star_qdt"]/li/a/img
-    soup = BeautifulSoup(content, 'lxml')
-    a = soup.find_all(attrs={'class': 'avatar-box'})
-    d = {}
-    for i in a:
-        s = i.img['src']
-        t = i.span.get_text()
-        p2 = {t: s}
-        d.update(p2)
-    return d
+from avdc.utility.metadata import Metadata
+from avdc.utility.misc import extractTitle
 
 
 def getTitle(content: str) -> str:
-    try:
-        html = etree.fromstring(content, etree.HTMLParser())
-        result = str(html.xpath('/html/body/div[2]/h3/text()')).strip(" ['']")  # [0]
-        return result.replace('/', '')
-    except:
-        return ''
+    tree = etree.fromstring(content, etree.HTMLParser())
+    title = str(tree.xpath('/html/body/div[2]/h3/text()')).strip(" ['']")  # [0]
+    return extractTitle(title)
 
 
 def getStars(content: str) -> list[str]:  # //*[@id="center_column"]/div[2]/div[1]/div/table/tbody/tr[1]/td/text()
@@ -36,45 +22,45 @@ def getStars(content: str) -> list[str]:  # //*[@id="center_column"]/div[2]/div[
 
 
 def getStudio(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result = str(html.xpath('//p[contains(text(),"制作商: ")]/following-sibling::p[1]/a/text()')).strip(" ['']").replace(
-        "', '", ' ')
+    tree = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
+    result = str(tree.xpath('//p[contains(text(),"制作商: ")]/following-sibling::p[1]/a/text()')) \
+        .strip(" ['']").replace("', '", ' ')
     return result
 
 
 def getRuntime(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result = str(html.xpath('//span[contains(text(),"长度:")]/../text()')).strip(" ['分钟']")
+    tree = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
+    result = str(tree.xpath('//span[contains(text(),"长度:")]/../text()')).strip(" ['分钟']")
     return result
 
 
 def getLabel(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result = str(html.xpath('//p[contains(text(),"系列:")]/following-sibling::p[1]/a/text()')).strip(" ['']")
+    tree = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
+    result = str(tree.xpath('//p[contains(text(),"系列:")]/following-sibling::p[1]/a/text()')).strip(" ['']")
     return result
 
 
 def getID(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result = str(html.xpath('//span[contains(text(),"识别码:")]/../span[2]/text()')).strip(" ['']")
+    tree = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
+    result = str(tree.xpath('//span[contains(text(),"识别码:")]/../span[2]/text()')).strip(" ['']")
     return result
 
 
 def getRelease(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result = str(html.xpath('//span[contains(text(),"发行时间:")]/../text()')).strip(" ['']")
+    tree = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
+    result = str(tree.xpath('//span[contains(text(),"发行时间:")]/../text()')).strip(" ['']")
     return result
 
 
 def getCover(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())
-    result = str(html.xpath('/html/body/div[2]/div[1]/div[1]/a/img/@src')).strip(" ['']")
+    tree = etree.fromstring(content, etree.HTMLParser())
+    result = str(tree.xpath('/html/body/div[2]/div[1]/div[1]/a/img/@src')).strip(" ['']")
     return result
 
 
 def getSmallCover(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())
-    result = str(html.xpath('//*[@id="waterfall"]/div/a/div[1]/img/@src')).strip(" ['']")
+    tree = etree.fromstring(content, etree.HTMLParser())
+    result = str(tree.xpath('//*[@id="waterfall"]/div/a/div[1]/img/@src')).strip(" ['']")
     return result
 
 
@@ -88,52 +74,42 @@ def getTags(content: str) -> list[str]:
 
 
 def getSeries(content: str) -> str:
-    try:
-        html = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-        result = str(html.xpath('//span[contains(text(),"系列:")]/../span[2]/text()')).strip(" ['']")
-        return result
-    except:
-        return ''
+    tree = etree.fromstring(content, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
+    return str(tree.xpath('//span[contains(text(),"系列:")]/../span[2]/text()')).strip(" ['']")
 
 
-@toMetadata
-def main(number: str) -> dict:
-    html = get_html('https://tellme.pw/avsox')
-    site = etree.HTML(html).xpath('//div[@class="container"]/div/a/@href')[0]
-    a = get_html(site + '/cn/search/' + number)
-    html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result = str(html.xpath('//*[@id="waterfall"]/div/a/@href')).strip(" ['']")
-    if result == '' or result == 'null' or result == 'None':
-        a = get_html(site + '/cn/search/' + number.replace('-', '_'))
-        html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-        result = str(html.xpath('//*[@id="waterfall"]/div/a/@href')).strip(" ['']")
-        if result == '' or result == 'null' or result == 'None':
-            a = get_html(site + '/cn/search/' + number.replace('_', ''))
-            html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-            result = str(html.xpath('//*[@id="waterfall"]/div/a/@href')).strip(" ['']")
-    web = get_html(result)
-    soup = BeautifulSoup(web, 'lxml')
+def main(keyword: str) -> Metadata:
+    text = get_html('https://tellme.pw/avsox')
+    avsox_site = etree.HTML(text).xpath('//div[@class="container"]/div/a/@href')[0]
+
+    def search_url(v):
+        x = get_html(avsox_site + '/cn/search/' + v)
+        tree = etree.fromstring(x, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
+        return str(tree.xpath('//*[@id="waterfall"]/div/a/@href')).strip(" ['']"), x
+
+    url, search_page = search_url(keyword)
+
+    text = get_html(url, raise_for_status=True)
+    soup = BeautifulSoup(text, 'lxml')
     info = str(soup.find(attrs={'class': 'row movie'}))
 
-    metadata = {
-        'stars': getStars(web),
-        'title': getTitle(web).strip(getID(web)).strip(),
+    return Metadata({
+        'stars': getStars(text),
+        'title': getTitle(text),
         'studio': getStudio(info),
         'overview': '',  #
         'runtime': getRuntime(info),
         'director': '',  #
         'release': getRelease(info),
         'id': getID(info),
-        'cover': getCover(web),
-        'small_cover': getSmallCover(a),
-        'tags': getTags(web),
+        'cover': getCover(text),
+        'small_cover': getSmallCover(search_page),
+        'tags': getTags(text),
         'label': getLabel(info),
-        'star_photos': getActorPhoto(web),
-        'website': result,
+        'website': url,
         'source': 'avsox',
         'series': getSeries(info),
-    }
-    return metadata
+    })
 
 
 if __name__ == "__main__":
