2bd88d1689214d9ac86019a98ae0a7a4c05e07e8
xjasonlyu <xjasonlyu@gmail.com>
2021-03-25 20:32:13 +0800

    about to fix mgstage

--- avdc/provider/mgstage.py
+++ avdc/provider/mgstage.py
@@ -9,110 +9,90 @@ from avdc.utility.metadata import Metadata
 
 def getTitle(a: str) -> str:
     html = etree.fromstring(a, etree.HTMLParser())
-    return str(html.xpath('//*[@id="center_column"]/div[1]/h1/text()')[0]).strip()
+    return html.xpath('//*[@id="center_column"]/div[1]/h1/text()')[0].strip()
 
 
 def getActresses(a: str) -> list[str]:  # //*[@id="center_column"]/div[2]/div[1]/div/table/tbody/tr[1]/td/text()
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"出演：")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"出演：")]/../td/text()')).strip(" ['']").strip('\\n    ').strip('\\n')
-    actresses = str(result1 + result2).strip('+').replace("', '", '').replace('"', '').replace('/', ',').replace('\\n', '')
-    return [i.strip() for i in actresses.split() if i.strip()]
+    result1 = html.xpath('//th[contains(text(),"出演：")]/../td/text()')
+    result2 = html.xpath('//th[contains(text(),"出演：")]/../td/a/text()')
+
+    return [k.strip() for j in {i.strip() for i in result1 + result2 if i.strip()}
+            for k in j.split() if k.strip()]
 
 
 def getStudio(a: str) -> str:
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"メーカー：")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"メーカー：")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    return str(result1 + result2).strip('+').replace("', '", '').replace('"', '').strip()
+    result1 = html.xpath('//th[contains(text(),"メーカー：")]/../td/a/text()')
+    result2 = html.xpath('//th[contains(text(),"メーカー：")]/../td/text()')
+    result = list({i.strip() for i in result1 + result2 if i.strip()})
+    return result[0] if result2 else ''
 
 
 def getRuntime(a: str) -> str:
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"収録時間：")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"収録時間：")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    return str(result1 + result2).strip('+').rstrip('mi').strip()
+    result1 = html.xpath('//th[contains(text(),"収録時間：")]/../td/a/text()')
+    result2 = html.xpath('//th[contains(text(),"収録時間：")]/../td/text()')
+    result: list[str] = [i.strip() for i in result1 + result2 if i.strip()]
+    return result[0].removesuffix('min') if result else ''
 
 
 def getLabel(a: str) -> str:
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"レーベル：")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"レーベル：")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    return str(result1 + result2).strip('+').replace("', '", '').replace('"', '').strip()
+    result1 = html.xpath('//th[contains(text(),"レーベル：")]/../td/a/text()')
+    result2 = html.xpath('//th[contains(text(),"レーベル：")]/../td/text()')
+    result = [i.strip() for i in result1 + result2 if i.strip()]
+    return result[0] if result else ''
 
 
 def getVID(a: str) -> str:
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"品番：")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"品番：")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    return str(result1 + result2).strip('+')
+    result1 = html.xpath('//th[contains(text(),"品番：")]/../td/a/text()')
+    result2 = html.xpath('//th[contains(text(),"品番：")]/../td/text()')
+    result = [i.strip() for i in result1 + result2 if i.strip()]
+    return result[0] if result else ''
 
 
 def getRelease(a: str) -> str:
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"配信開始日：")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"配信開始日：")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    return str(result1 + result2).strip('+').replace('/', '-')
+    result1 = html.xpath('//th[contains(text(),"配信開始日：")]/../td/a/text()')
+    result2 = html.xpath('//th[contains(text(),"配信開始日：")]/../td/text()')
+    result = [i.strip().replace('/', '-') for i in result1 + result2 if i.strip()]
+    return result[0] if result else ''
 
 
 def getGenres(a: str) -> list[str]:
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"ジャンル：")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"ジャンル：")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result = str(result1 + result2).strip('+').replace("', '\\n", ",").replace("', '", ""). \
-        replace('"', '').replace(',,', '').split(',')
-    return [i.strip() for i in result if i.strip()]
+    result1 = html.xpath('//th[contains(text(),"ジャンル：")]/../td/a/text()')
+    result2 = html.xpath('//th[contains(text(),"ジャンル：")]/../td/text()')
+    return [i.strip() for i in result1 + result2 if i.strip()]
 
 
 def getCover(content: str) -> str:
     html = etree.fromstring(content, etree.HTMLParser())
-    result = str(html.xpath('//*[@id="EnlargeImage"]/@href')).strip(" ['']")
-    #                    //*[@id="EnlargeImage"]
-    return result
+    result: list[str] = html.xpath('//*[@id="EnlargeImage"]/@href')
+    return result[0].strip() if result else ''
 
 
-def getSmallCover(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())
-    result = str(html.xpath('//*[@id="center_column"]/div[1]/div[1]/div/div/h2/img/@src')).strip(" ['']")
-    #                    /html/body/div[2]/article[2]/div[1]/div[1]/div/div/h2/img/@src
-    return result
-
-
-# def getDirector(a: str) -> str:
-#     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-#     result1 = str(html.xpath('//th[contains(text(),"メーカー")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-#         '\\n')
-#     result2 = str(html.xpath('//th[contains(text(),"メーカー")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-#         '\\n')
-#     return str(result1 + result2).strip('+').replace("', '", '').replace('"', '').strip()
-
-
-def getOverview(content: str) -> str:
-    html = etree.fromstring(content, etree.HTMLParser())
-    result = str(html.xpath('//p/text()')).strip(" ['']").replace(u'\\n', '').replace("', '', '", '')
-    return result
+# def getSmallCover(content: str) -> str:
+#     html = etree.fromstring(content, etree.HTMLParser())
+#     result = str(html.xpath('//*[@id="center_column"]/div[1]/div[1]/div/div/h2/img/@src')).strip(" ['']")
+#     #                    /html/body/div[2]/article[2]/div[1]/div[1]/div/div/h2/img/@src
+#     return result
 
 
 def getSeries(a: str) -> str:
     html = etree.fromstring(a, etree.HTMLParser())  # //table/tr[1]/td[1]/text()
-    result1 = str(html.xpath('//th[contains(text(),"シリーズ")]/../td/a/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    result2 = str(html.xpath('//th[contains(text(),"シリーズ")]/../td/text()')).strip(" ['']").strip('\\n    ').strip(
-        '\\n')
-    return str(result1 + result2).strip('+').replace("', '", '').replace('"', '')
+    result1 = html.xpath('//th[contains(text(),"シリーズ")]/../td/a/text()')
+    result2 = html.xpath('//th[contains(text(),"シリーズ")]/../td/text()')
+    result = [i.strip() for i in result1 + result2 if i.strip()]
+    return result[0] if result else ''
+
+
+def getOverview(content: str) -> str:
+    html = etree.fromstring(content, etree.HTMLParser())
+    result: list[str] = html.xpath('//p/text()')
+    return result[0].strip() if result else ''
 
 
 def getImages(content: str) -> list[str]:  # 获取剧照
@@ -132,19 +112,11 @@ def main(keyword: str) -> Metadata:
     text = str(get_html(url, cookies={'adc': '1'}))
 
     soup = BeautifulSoup(text, 'lxml')
-    a = str(soup.find(attrs={'class': 'detail_data'})). \
-        replace('\n                                        ', ''). \
-        replace('                                ', ''). \
-        replace('\n                            ', ''). \
-        replace('\n                        ', '')
-    b = str(soup.find(attrs={"id": "introduction"})). \
-        replace('\n                                        ', ''). \
-        replace('                                ', ''). \
-        replace('\n                            ', ''). \
-        replace('\n                        ', '')
+    a = str(soup.find(attrs={'class': 'detail_data'}))
+    b = str(soup.find(attrs={"id": "introduction"}))
 
     return Metadata({
-        'title': getTitle(text).replace("\\n", '').replace('        ', ''),
+        'title': getTitle(text),
         'studio': getStudio(a),
         'overview': getOverview(b),
         'runtime': getRuntime(a),
@@ -167,4 +139,4 @@ def main(keyword: str) -> Metadata:
 if __name__ == '__main__':
     # print(main('422ion-0062'))
     # print(main('AVOP-008'))
-    print(main('259LUXU-1377'))
+    print(main('485GCB-011'))
