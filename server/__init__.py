86fdc689613c74efb55f54d8bb8a2ca5560cb5d5
xjasonlyu <xjasonlyu@gmail.com>
2021-04-12 19:55:10 +0800

    independent app

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,13 @@
+import os
 from importlib import import_module
 
 from flask import Flask
 
 app = Flask(__name__)
 
-# init
+# init app
+app.config.setdefault('DATABASE', os.environ.get('AVDC_DATABASE'))
+app.config.setdefault('TOKEN', os.environ.get('AVDC_TOKEN'))
+
+# init module
 import_module('.views', package=__name__)
